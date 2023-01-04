2edec156d97c41600f470095a76a1534f1d7a1d1
Ben Casson <bcasson11@gmail.com>
2023-01-04 09:26:00 -0600

    Enable cors

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+//Enable cors
+app.use(cors());
+
 //Routes
 app.use('/api', require('./routes/foodList'))
 
 
-//Enable cors
-app.use(cors({
-    origin: '*'
-}));
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
