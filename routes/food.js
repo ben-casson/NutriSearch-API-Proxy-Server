1a78a106cdd60608882a3129562a8d96fcf4ac31
Ben Casson <bcasson11@gmail.com>
2023-01-04 09:37:51 -0600

    Create food router

---
+++ routes/food.js
@@ -0,0 +1,27 @@
+const url = require('url');
+const express = require('express');
+const router = express.Router();
+const needle = require('needle');
+
+//Environment variables
+const API_FOOD_BASE_URL = process.env.API_FOOD_BASE_URL;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
+
+router.get('/', async (req, res) => {
+    const params = new URLSearchParams({
+        ...url.parse(req.url, true).query,
+        [API_KEY_NAME]: API_KEY_VALUE
+    });
+
+    try {
+        const apiRes = await needle('get', `${API_FOOD_BASE_URL}?${params}`);
+        const data = apiRes.body;
+
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+});
+
+module.exports = router;
