0698d7acbf3db3bdc3d9d9a18ad7f209903eb3de
Ben Casson <bcasson11@gmail.com>
2023-01-04 13:40:06 -0600

    Edit GET path

--- routes/foodItem.js
+++ routes/foodItem.js
@@ -8,16 +8,16 @@ const API_FOOD_BASE_URL = process.env.API_FOOD_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
-router.get('/', async (req, res) => {
-    const params = new URLSearchParams({
-        ...url.parse(req.url, true).query,
-    });
-    console.log(params)
+router.get('/:id', async (req, res) => {
+    // const params = new URLSearchParams({
+    //     ...url.parse(req.url, true).query,
+    // });
+    console.log(req.params.id)
 
     try {
         const apiRes = await needle(
             'get',
-            `${API_FOOD_BASE_URL}/${params}?${API_KEY_NAME}=${API_KEY_VALUE}`
+            `${API_FOOD_BASE_URL}/${req.params.id}?${API_KEY_NAME}=${API_KEY_VALUE}`
         );
         const data = apiRes.body;
 
