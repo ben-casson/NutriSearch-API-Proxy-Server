f57f317dae0b23d8da011a2cb645bbd6258f0135
Ben Casson <bcasson11@gmail.com>
2023-01-04 12:44:28 -0600

    Change GET url

--- routes/foodItem.js
+++ routes/foodItem.js
@@ -8,21 +8,16 @@ const API_FOOD_BASE_URL = process.env.API_FOOD_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
-let id = '454004';
-
-router.post('/', (req, res) => {
-    id = req.body.id; // your implementation here
-    res.send('Saved!');
-});
-
 router.get('/', async (req, res) => {
     const params = new URLSearchParams({
         // ...url.parse(req.url, true).search,
         [API_KEY_NAME]: API_KEY_VALUE,
     });
 
+    let food = req.query.food;
+
     try {
-        const apiRes = await needle('get', `${API_FOOD_BASE_URL}${id}?${params}`);
+        const apiRes = await needle('get', `${API_FOOD_BASE_URL}${food}?${params}`);
         const data = apiRes.body;
 
         res.status(200).json(data);
