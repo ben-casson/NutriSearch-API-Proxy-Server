78bd6168f75c254355144dc9291a33ae52a45081
Ben Casson <bcasson11@gmail.com>
2023-01-04 13:18:57 -0600

    Add query parameter to url

--- routes/foodItem.js
+++ routes/foodItem.js
@@ -9,15 +9,15 @@ const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
 router.get('/', async (req, res) => {
-    const params = new URLSearchParams({
-        ...url.parse(req.url, true).query
-        // [API_KEY_NAME]: API_KEY_VALUE,
-    });
+    // const params = new URLSearchParams({
+    //     ...url.parse(req.url, true).query
+    //     [API_KEY_NAME]: API_KEY_VALUE,
+    // });
 
-    let food = req.query.food;
+    let query = url.parse(req.url, true).query;
 
     try {
-        const apiRes = await needle('get', `${API_FOOD_BASE_URL}${params}?${API_KEY_NAME}=${API_KEY_VALUE}`);
+        const apiRes = await needle('get', `${API_FOOD_BASE_URL}${query}?${API_KEY_NAME}=${API_KEY_VALUE}`);
         const data = apiRes.body;
 
         res.status(200).json(data);
