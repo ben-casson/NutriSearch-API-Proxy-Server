6989394c63878e0bb20337b2fb348555e70454a0
Ben Casson <bcasson11@gmail.com>
2023-01-13 09:16:59 -0600

    Use apicache

--- routes/foodItem.js
+++ routes/foodItem.js
@@ -1,14 +1,16 @@
-const url = require('url');
 const express = require('express');
 const router = express.Router();
 const needle = require('needle');
+const apicache = require('apicache');
 
 //Environment variables
 const API_FOOD_BASE_URL = process.env.API_FOOD_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
-router.get('/:id', async (req, res) => {
+let cache = apicache.middleware;
+
+router.get('/:id', cache('1 day'), async (req, res) => {
     try {
         const apiRes = await needle(
             'get',
--- routes/foodList.js
+++ routes/foodList.js
@@ -2,13 +2,16 @@ const url = require('url');
 const express = require('express');
 const router = express.Router();
 const needle = require('needle');
+const apicache = require('apicache');
 
 //Environment variables
 const API_FOOD_LIST_BASE_URL = process.env.API_FOOD_LIST_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
-router.get('/', async (req, res) => {
+let cache = apicache.middleware
+
+router.get('/', cache('1 day'), async (req, res) => {
     const params = new URLSearchParams({
         [API_KEY_NAME]: API_KEY_VALUE,
         ...url.parse(req.url, true).query
